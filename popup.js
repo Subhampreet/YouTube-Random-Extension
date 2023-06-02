1a4a51e333f7b5c8282c62bfde76659e3b20a564
Subhampreet <subhampreet18@gmail.com>
2023-06-03 03:12:56 +0530

    YouTube Randomize basic functionality v1

---
+++ popup.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var randomVideoButton = document.getElementById('random-video-button');
+    var videoContainer = document.getElementById('video-container');
+
+    randomVideoButton.addEventListener('click', function() {
+        getRandomVideo();
+    });
+
+    async function getRandomVideo() {
+        const response = await fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyARYHrVA3gNfk-yM4nLAoMmG5hRrhEoppw&part=snippet&maxResults=1&q=funny');
+        const data = await response.json();
+        
+        if (data.items && data.items.length > 0) {
+          const videoId = data.items[0].id.videoId;
+          const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+          window.open(videoUrl, '_blank');
+        }
+    }
+
+    //   // Add a click event listener to the browser action button
+    //chrome.action.onClicked.addListener(getRandomVideo);
+
+    //// AXIOS Implementation
+    // function getRandomVideo() {
+
+    //     const axios = require('axios');
+
+    //     axios.get('https://www.googleapis.com/youtube/v3/search', {
+    //         params: {
+    //             part: 'snippet',
+    //             maxResults: 1,
+    //             q: 'funny',
+    //             type: 'video',
+    //             key: 'AIzaSyARYHrVA3gNfk-yM4nLAoMmG5hRrhEoppw'
+    //         }
+    //     })
+    //     .then(function(response) {
+    //         var videoId = response.data.items[0].id.videoId;
+    //         var videoUrl = 'https://www.youtube.com/watch?v=' + videoId;
+
+    //         videoContainer.innerHTML = '<iframe width="560" height="315" src="' + videoUrl + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+    //     })
+    //     .catch(function(error) {
+    //         console.log(error);
+    //     });
+    // }
+});
+
+
+  
+//   // Add a click event listener to the browser action button
+//   chrome.action.onClicked.addListener(getRandomVideo);
