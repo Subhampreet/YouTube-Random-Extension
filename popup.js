735fb6e64626203c99de5c85380288d519f9a742
Subhampreet <subhampreet18@gmail.com>
2023-06-03 03:31:31 +0530

    v2: fetch data using genre functionality

--- popup.js
+++ popup.js
@@ -1,52 +1,106 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var randomVideoButton = document.getElementById('random-video-button');
-    var videoContainer = document.getElementById('video-container');
+// document.addEventListener('DOMContentLoaded', function() {
+//     var randomVideoButton = document.getElementById('random-video-button');
+//     var videoContainer = document.getElementById('video-container');
 
-    randomVideoButton.addEventListener('click', function() {
-        getRandomVideo();
-    });
+//     randomVideoButton.addEventListener('click', function() {
+//         getRandomVideo();
+//     });
 
-    async function getRandomVideo() {
-        const response = await fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyARYHrVA3gNfk-yM4nLAoMmG5hRrhEoppw&part=snippet&maxResults=1&q=funny');
-        const data = await response.json();
+//     async function getRandomVideo(genre) {
+//         const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyARYHrVA3gNfk-yM4nLAoMmG5hRrhEoppw&part=snippet&maxResults=1&q=${genre} music`);
+//         const data = await response.json();
         
-        if (data.items && data.items.length > 0) {
-          const videoId = data.items[0].id.videoId;
-          const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
-          window.open(videoUrl, '_blank');
-        }
-    }
+//         if (data.items && data.items.length > 0) {
+//           const videoId = data.items[0].id.videoId;
+//           const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+//           window.open(videoUrl, '_blank');
+//         }
+//       }
+      
+//       // Function to handle the button click event
+//       function handleButtonClick() {
+//         const genreInput = document.getElementById('genre');
+//         const genre = genreInput.value.trim();
+        
+//         if (genre) {
+//           getRandomVideo(genre);
+//         }
+//       }
+      
+//       // Add click event listener to the button
+//       document.getElementById('fetchBtn').addEventListener('click', handleButtonClick);
 
-    //   // Add a click event listener to the browser action button
-    //chrome.action.onClicked.addListener(getRandomVideo);
+//     // async function getRandomVideo() {
+//     //     const response = await fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyARYHrVA3gNfk-yM4nLAoMmG5hRrhEoppw&part=snippet&maxResults=1&q=funny');
+//     //     const data = await response.json();
+        
+//     //     if (data.items && data.items.length > 0) {
+//     //       const videoId = data.items[0].id.videoId;
+//     //       const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+//     //       window.open(videoUrl, '_blank');
+//     //     }
+//     // }
 
-    //// AXIOS Implementation
-    // function getRandomVideo() {
+//     //   // Add a click event listener to the browser action button
+//     //chrome.action.onClicked.addListener(getRandomVideo);
 
-    //     const axios = require('axios');
+//     //// AXIOS Implementation
+//     // function getRandomVideo() {
 
-    //     axios.get('https://www.googleapis.com/youtube/v3/search', {
-    //         params: {
-    //             part: 'snippet',
-    //             maxResults: 1,
-    //             q: 'funny',
-    //             type: 'video',
-    //             key: 'AIzaSyARYHrVA3gNfk-yM4nLAoMmG5hRrhEoppw'
-    //         }
-    //     })
-    //     .then(function(response) {
-    //         var videoId = response.data.items[0].id.videoId;
-    //         var videoUrl = 'https://www.youtube.com/watch?v=' + videoId;
+//     //     const axios = require('axios');
 
-    //         videoContainer.innerHTML = '<iframe width="560" height="315" src="' + videoUrl + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
-    //     })
-    //     .catch(function(error) {
-    //         console.log(error);
-    //     });
-    // }
-});
+//     //     axios.get('https://www.googleapis.com/youtube/v3/search', {
+//     //         params: {
+//     //             part: 'snippet',
+//     //             maxResults: 1,
+//     //             q: 'funny',
+//     //             type: 'video',
+//     //             key: 'AIzaSyARYHrVA3gNfk-yM4nLAoMmG5hRrhEoppw'
+//     //         }
+//     //     })
+//     //     .then(function(response) {
+//     //         var videoId = response.data.items[0].id.videoId;
+//     //         var videoUrl = 'https://www.youtube.com/watch?v=' + videoId;
+
+//     //         videoContainer.innerHTML = '<iframe width="560" height="315" src="' + videoUrl + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+//     //     })
+//     //     .catch(function(error) {
+//     //         console.log(error);
+//     //     });
+//     // }
+// });
+
+
+// // Function to fetch a random YouTube video based on a given category
+
 
 
   
-//   // Add a click event listener to the browser action button
-//   chrome.action.onClicked.addListener(getRandomVideo);
+// //   // Add a click event listener to the browser action button
+// //   chrome.action.onClicked.addListener(getRandomVideo);
+
+
+// Function to fetch a random YouTube video based on a given genre
+async function getRandomVideo(genre) {
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyARYHrVA3gNfk-yM4nLAoMmG5hRrhEoppw&part=snippet&maxResults=1&q=${genre} music`);
+    const data = await response.json();
+    
+    if (data.items && data.items.length > 0) {
+      const videoId = data.items[0].id.videoId;
+      const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+      window.open(videoUrl, '_blank');
+    }
+  }
+  
+  // Function to handle the button click event
+  function handleButtonClick() {
+    const genreInput = document.getElementById('genre');
+    const genre = genreInput.value.trim();
+    
+    if (genre) {
+      getRandomVideo(genre);
+    }
+  }
+  
+  // Add click event listener to the button
+  document.getElementById('fetchBtn').addEventListener('click', handleButtonClick);
